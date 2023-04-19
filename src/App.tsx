0a889e0f27e43fd6c7518156b0fb8309b03b72f3
Alexandre Kaimer <alekaimer@gmail.com>
2023-04-19 18:06:18 -0300

    refactor: Css arch

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.scss'
-
-interface Character {
-  name: string
-  dateOfBirth: string
-  house: string
-  patronus: string
-  actor: string
-  alive: boolean
-  image: string
-}
+import Card from './components/Card/Card'
+import { ICharacter } from './dtos/ICharacter.dto'
 
 function App() {
-  const [characters, setCharacters] = useState<Character[]>([])
+  const [characters, setCharacters] = useState<ICharacter[]>([])
   const [loading, setLoading] = useState(false)
+  const [errors, setErrors] = useState<string>('')
 
   async function getCharacters() {
     setLoading(true)
-    // const response = await fetch('https://hp-api.herokuapp.com/api/characters') //TODO: Testar com URL inválida
-    const response = await fetch('https://hp-api.onrender.com/api/characters')
-    const data = await response.json()
-    setCharacters(data)
-    setLoading(false)
+
+    try {
+      const response = await fetch(
+        // 'https://hp-api.herokuapp.com/api/characters',
+        'https://hp-api.onrender.com/api/characters',
+      )
+      const data = await response.json()
+      setCharacters(data)
+      setLoading(false)
+    } catch (error: unknown) {
+      setErrors('Não foi possível carregar os personagens')
+      setLoading(false)
+    }
   }
 
+  useEffect(() => {
+    characters.length < 1 && getCharacters()
+  }, [characters])
+
+  // return early while loading
   if (loading) {
     return <h1>Loading...</h1>
+  }
+
+  // return early if there are no characters
+  if (characters.length < 1) {
+    return <h1>No characters found</h1>
+  }
+
+  // return early if there are errors
+  if (errors) {
+    return <h1>{errors}</h1>
   }
 
   return (
     <div className="App">
       <h1>Harry Potter's Landing Page</h1>
-      <button onClick={getCharacters}>Get Characters</button>
-      <ul>
-        {characters.map((character) => (
-          <li key={character.name}>
-            <img src={character.image} alt={character.name} />
-            <h2>{character.name}</h2>
-            <p>
-              Actor: <b>{character.actor}</b>
-            </p>
-            <p>House: {character.house}</p>
-            <p>Patronus: {character.patronus}</p>
-            <p>Date of Birth: {character.dateOfBirth}</p>
-            <p>Alive: {character.alive ? 'Yes' : 'No'}</p>
-          </li>
-        ))}
+      <ul className="grid">
+        {!errors ? (
+          characters.map((character) => (
+            <li key={character.id}>
+              <Card data={character} />
+            </li>
+          ))
+        ) : (
+          <h1>{errors}</h1>
+        )}
       </ul>
     </div>
   )
